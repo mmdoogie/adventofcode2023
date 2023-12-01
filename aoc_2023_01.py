c961b5ccc36274c176a75590bc3807a35c2c003c
Matthew McDougal <24367033+mmdoogie@users.noreply.github.com>
2023-12-01 00:56:22 -0600

    Improve Day 1

--- aoc_2023_01.py
+++ aoc_2023_01.py
@@ -15,6 +15,7 @@ def part1(output = True):
                 break
 
         cal_sum += int(c1 + c2)
+
         if output:
             i2 = len(d) - 1 - i2
             print(d[:i1] + ansi.blue(d[i1]) + d[i1 + 1:i2] +
@@ -23,30 +24,24 @@ def part1(output = True):
     return cal_sum
 
 def part2(output = True):
-    digits = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
-                '1',   '2',     '3',    '4',    '5',   '6',     '7',     '8',    '9']
-    reversed_digits = [''.join(reversed(d)) for d in digits]
-
-    conv = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9,
-              '1': 1,   '2': 2,     '3': 3,    '4': 4,    '5': 5,   '6': 6,     '7': 7,     '8': 8,    '9': 9}
-    conv.update({rd: conv[d] for d, rd in zip(digits, reversed_digits)})
-    digits += reversed_digits
+    digits = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9,
+                '1': 1,   '2': 2,     '3': 3,    '4': 4,    '5': 5,   '6': 6,     '7': 7,     '8': 8,    '9': 9}
 
     cal_sum = 0
     for d in dat:
-        idxs = {d.index(x): x for x in digits if x in d}
+        idxs = {d.index(k): k for k, v in digits.items() if k in d}
         c1 = min(idxs.items())
 
-        reversed_d = ''.join(reversed(d))
-        idxs = {reversed_d.index(x): x for x in digits if x in reversed_d}
-        c2 = min(idxs.items())
-        cal_sum += conv[c1[1]] * 10 + conv[c2[1]]
+        idxs = {d.rindex(k): k for k, v in digits.items() if k in d}
+        c2 = max(idxs.items())
+
+        cal_sum += digits[c1[1]] * 10 + digits[c2[1]]
 
         if output:
             i1_s = c1[0]
             i1_e = c1[0] + len(c1[1])
-            i2_e = len(d) - c2[0]
-            i2_s = len(d) - c2[0] - len(c2[1])
+            i2_s = c2[0]
+            i2_e = c2[0] + len(c2[1])
             print(d[:i1_s] + ansi.blue(d[i1_s:i1_e]) + d[i1_e:i2_s] +
                             (ansi.blue(d[i2_s:i2_e]) if i1_s != i2_s else '') + d[i2_e:])
 
